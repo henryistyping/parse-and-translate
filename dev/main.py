8916a796f21c6d000a0fc46947a3a3587f4d52d4
henryistyping <mykim1891@gmail.com>
2025-01-15 22:02:28 -0800

    file name clean up

--- dev/main.py
+++ dev/main.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(1, "./main_screen_ui")
 
 from PySide6 import QtWidgets
-from mainscreen_logic import MainScreen
+from main_screen_logic import MainScreen
 
 app = QtWidgets.QApplication(sys.argv)
 
