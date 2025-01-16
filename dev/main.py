b9f921c9db02ad84ed2a144a86dce44ce6286a5a
henryistyping <mykim1891@gmail.com>
2025-01-15 23:37:23 -0800

    parses each row of strings into 1st column of the row

--- dev/main.py
+++ dev/main.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.insert(1, "./main_screen_ui")
+sys.path.insert(1, "./main_screen_ui") # adds path for module in diff dir
 
 from PySide6 import QtWidgets
 from main_screen_logic import MainScreen
--- dev/main_screen_logic.py
+++ dev/main_screen_logic.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget, QFileDialog, QMainWindow
+from PySide6.QtWidgets import QWidget, QFileDialog, QMainWindow, QTableWidgetItem
 from main_screen_ui import Ui_MainWindow
 
 
@@ -8,7 +8,33 @@ class MainScreen(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("Super Localizer")
-        self.actionOpen.triggered.connect(self.do_something)
+        self.actionOpen.triggered.connect(
+            self.browseFiles
+        )  #://FIXME ask to save the current file bofore opening dialog window.
 
     def do_something(self):
         print("Hello, World!")
+
+    def browseFiles(self):
+        fileName = QFileDialog.getOpenFileName(
+            self, "Open File", "", "All Files (*);;Text Files (*.txt)"
+        )
+
+        while self.mainTableWidget.rowCount() > 0:
+            self.mainTableWidget.removeRow(0)
+
+        if fileName[0]:
+            f = open(fileName[0], "r")
+
+            with f:
+                data = f.readlines()
+                self.populateTable(data)
+
+    def populateTable(self, data):
+        n = self.mainTableWidget.rowCount()
+        self.mainTableWidget.setRowCount(n + len(data))  # hardcoded for now
+        for i in data:
+            item = item2 = QTableWidgetItem()
+            self.mainTableWidget.setItem(n, 0, item)  # set source text
+            item.setText(i)
+            n = n + 1
